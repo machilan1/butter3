c5cb9a475a79d2098f06f3b621dbe7d911658b91
machilan1 <machilan1@gmail.com>
2023-02-09 13:06:20 +0800

    some touches

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@
 // set sidebar focus
 const sideTagArea = document.querySelector(".sidetag-area");
 const cartIcon = document.querySelector('.icon-bag-svg');
-const cartArea = document.querySelector('.cart-area');
+const cartBackground = document.querySelector('.cart-background');
+const cartContent = document.querySelector('.cart-content');
 const sideTagAreaChild = n=>document.querySelector(".sidetag-area").children[n]
 const closeBtn = document.querySelector('.close-button');
+const cartArea =document.querySelector('.cart-area');
 
 for(let i =0; i<sideTagArea.children.length;i++){
     sideTagAreaChild(i).addEventListener('click',()=>{fullpage_api.moveTo(`${i+1}`,0)})
@@ -14,13 +16,14 @@ for(let i =0; i<sideTagArea.children.length;i++){
 
 // cart on/off
 cartIcon.addEventListener('click',()=>{
-    cartArea.classList.add('show-cart');
-    cartArea.children[0].classList.add('show-cart-content');
+    cartBackground.classList.add('show-cart');
+    cartContent.classList.add('show-cart-content');
 })
 
 cartArea.addEventListener('click',(e)=>{
-    if(e.target ==cartArea||e.target==closeBtn){
-        cartArea.classList.remove('show-cart');
-        cartArea.children[0].classList.remove('show-cart-content');
+    console.log(`${e.target} is clicked`)
+    if(e.target ==cartBackground||e.target==closeBtn){
+        cartBackground.classList.remove('show-cart');
+        cartContent.classList.remove('show-cart-content');
     }
 })
